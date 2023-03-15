0b72857ab6fe23f6012ca60ff57f88f9357967cf
Yuji Nakamura <ny5117@icloud.com>
2023-03-15 21:17:04 +0900

    ステータスのサンプルデータを作成

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,13 +3,67 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const habit = await prisma.habit.create({
-    data: {
-      title: 'ヨガ',
-    },
+  const habit = await prisma.habit.createMany({
+    data: [
+      {
+        title: 'ヨガ',
+      },
+      {
+        title: '瞑想',
+      },
+      {
+        title: '英語勉強',
+      },
+      {
+        title: 'ランニング',
+      },
+    ],
   });
 
-  console.log(habit);
+  const habitStatus = await prisma.habitStatus.createMany({
+    data: [
+      {
+        habitId: 1,
+        isCompleted: true,
+        targetedDate: '2023-03-15',
+      },
+      {
+        habitId: 1,
+        isCompleted: true,
+        targetedDate: '2023-03-16',
+      },
+      {
+        habitId: 1,
+        isCompleted: true,
+        targetedDate: '2023-03-17',
+      },
+      {
+        habitId: 2,
+        isCompleted: true,
+        targetedDate: '2023-03-15',
+      },
+      {
+        habitId: 2,
+        isCompleted: false,
+        targetedDate: '2023-03-16',
+      },
+      {
+        habitId: 2,
+        isCompleted: true,
+        targetedDate: '2023-03-17',
+      },
+      {
+        habitId: 3,
+        isCompleted: true,
+        targetedDate: '2023-03-10',
+      },
+      {
+        habitId: 4,
+        isCompleted: true,
+        targetedDate: '2023-03-25',
+      },
+    ],
+  });
 }
 
 main()
