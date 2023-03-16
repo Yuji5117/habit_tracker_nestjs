696381736202ba08271a192d549afda265923abe
Yuji Nakamura <ny5117@icloud.com>
2023-03-16 21:00:43 +0900

    middlewareにcorsの許可を実装

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CorsMiddleware } from './cors.middleware';
 import { HabitsModule } from './habits/habits.module';
 
 @Module({
@@ -8,4 +9,8 @@ import { HabitsModule } from './habits/habits.module';
   providers: [AppService],
   imports: [HabitsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorsMiddleware).forRoutes('*');
+  }
+}
---
+++ src/cors.middleware.ts
@@ -0,0 +1,15 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+
+    next();
+  }
+}
